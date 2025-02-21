32bdc5527aa12ad3b659f45731e0c89980ef1a55
kakarot987 <mayankit3@gmail.com>
2025-02-21 10:49:48 +0530

    local db

--- src/main/java/com/spring_webflux_docker/model/User.java
+++ src/main/java/com/spring_webflux_docker/model/User.java
@@ -4,6 +4,8 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,7 +13,7 @@ import org.springframework.data.relational.core.mapping.Table;
 @Table("users")
 public class User {
     @Id
-    private String id;
+    private UUID id;
     private String name;
     private String email;
 }
